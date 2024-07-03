1670467c6b65c56eecf5e4710c2a346e320df70d
Raj Aryan <rajisonline2306@gmail.com>
2024-07-04 01:18:55 +0530

    Next Greatest Element [Arrays]

---
+++ Arrays/NextGreatestElement.java
@@ -0,0 +1,20 @@
+public class NextGreatestElement {
+    public static void main(String[] args) {
+        int[] arr = {12,8,41,60,2,49,16,28,21};
+        int n=arr.length;
+        int[] ans = new int[n];
+        ans[n-1]=-1;
+        int NextGreatestElement = arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            ans[i]= NextGreatestElement;
+            NextGreatestElement = Math.max(NextGreatestElement, arr[i]);
+        }
+
+        System.out.println("NEW ARRAY: ");
+        for(int i=0;i<n;i++){
+            System.out.print(ans[i]+" ");
+        }
+
+    }
+    
+}
