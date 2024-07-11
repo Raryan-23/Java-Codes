b5cd704a423990b0d7a3268ad492c17d5d38b1f2
Raj Aryan <rajisonline2306@gmail.com>
2024-07-12 04:26:36 +0530

    Column wise print and store transpose in new array

---
+++ Arrays/ColumnWisePrinting.java
@@ -0,0 +1,28 @@
+public class ColumnWisePrinting {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4}, {5,6,7,8},{1,2,3,8}};
+        int n = arr.length;
+        int m = arr[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[j][i] + " ");
+                }
+                System.out.println();
+                }
+        // Storing the transpose into a different Array.
+        System.out.println();
+        int[][] arr2 = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr2[i][j] = arr[j][i];
+            }
+        }
+        //print elements of arr2
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+                System.out.println();
+        }
+    }
+}
